f227c3445453edc21d5d890b35f4a3b174b73a27
sa07m <127376011+sa07m@users.noreply.github.com>
2023-10-10 12:32:12 +0530

    includes user model and associations

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const User = require('../models/user')
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -13,11 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user
+  .createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
   })
     .then(result => {
       // console.log(result);
@@ -35,8 +37,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user.getProducts({where:{id:prodId}})
+  //Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0]  
       if (!product) {
         return res.redirect('/');
       }
@@ -72,7 +76,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
